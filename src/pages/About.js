1be5b3dd001f18e967a23e73e88d1d2923e50761
Zain Jakwani <zjakwani@gmail.com>
2021-11-14 11:46:01 -0700

    filled out about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,17 @@ const About = props => {
     return (
         <div>
             <Header title="About"/>
-            <p>Web App Created to inform users about National Parks.</p>
+            <p>
+            For the last 100 years, the National Park Service
+            has contributed greatly to the preservation and management
+            of America's national parks, as well as many of its national monuments.
+            More than 330 million people visit the 61 national parks they oversee each year,
+            with 40 million overnight campers. 
+            </p>
+            <p>
+                This app was created to inform and encourage viewers to visit National Parks.
+            </p>
+            <a href="https://github.com/zjakwani/parks">Github</a>
         </div>
     )
 }
